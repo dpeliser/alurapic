09c6d5ef8f06e7e123141d65bcdf53ab6ce3a93a
Diego Peliser <peliser.diego@gmail.com>
2020-12-07 00:08:06 -0300

    Angular parte 1: Fundamentos - Itens 1 ao 6.

---
+++ src/app/photos/photo/photo.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Photo } from './photo';
+
+const API = 'http://localhost:3000';
+
+@Injectable({ providedIn: 'root' })
+export class PhotoService {
+
+    constructor(private http: HttpClient) { }
+
+    listFromUser(user: string) {
+        return this.http.get<Photo[]>(`${API}/${user}/photos`);
+    }
+
+    listFromUserPaginated(user: string, page: number) {
+        const params = new HttpParams().append('page', page.toString());
+        return this.http.get<Photo[]>(`${API}/${user}/photos`, { params });
+    }
+
+}
